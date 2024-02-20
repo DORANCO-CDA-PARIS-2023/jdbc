e433b786c334a6f0b38d484ac9417824c9a0cb36
BuntProduction <buntlicorn@gmail.com>
2024-02-20 16:38:31 +0100

    V1 get the books borrowed by ID

---
+++ src/main/java/doranco/Student.java
@@ -0,0 +1,45 @@
+package doranco;
+
+public class Student {
+
+    // We cretae all the var
+    private int id;
+    private String name;
+    private String firstname;
+    private String studentNumber;
+
+    // The constructor
+    public Student(int id, String name, String firstname, String studentNumber) {
+        this.id = id;
+        this.name = name;
+        this.firstname = firstname;
+        this.studentNumber = studentNumber;
+    }
+
+    // The getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getStudentNumber() {
+        return studentNumber;
+    }
+
+    // The student object to get in the others scripts
+    public String toString() {
+        return "Student{" +
+                "ID=" + id +
+                ", Name='" + name +
+                ", Firstname='" + firstname +
+                ", Student Number='" + studentNumber +
+                '}';
+    } // to upgrade to get a better style
+}
