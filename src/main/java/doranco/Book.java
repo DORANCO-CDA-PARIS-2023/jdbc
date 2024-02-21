03b52f9f21d632b07bdff01b1d994dab35c55057
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-02-21 09:35:33 +0100

    Suppression erreur

--- src/main/java/doranco/Book.java
+++
@@ -1,9 +0,0 @@
-package doranco;
-
-public class Book {
-    private int id;
-    private String title;
-    private int yearPublish;
-    private int authorId;
-
-}
--- src/main/java/doranco/Main.java
+++
@@ -1,7 +0,0 @@
-package doranco;
-
-public class Main {
-    public static void main(String[] args) {
-        // Utilisez cette classe pour tester vos méthodes BookDAOImpl
-    }
-}
