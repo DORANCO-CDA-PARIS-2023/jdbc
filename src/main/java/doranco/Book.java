0444b1bdd0657bedca344705a1130e5b4fa7b9c0
Ryan Sifi <ryan.sifi.77@gmail.com>
2024-02-21 08:43:19 +0100

    Mise a jjour correction

---
+++ src/main/java/doranco/Book.java
@@ -0,0 +1,9 @@
+package doranco;
+
+public class Book {
+    private int id;
+    private String title;
+    private int yearPublish;
+    private int authorId;
+
+}
---
+++ src/main/java/doranco/Main.java
@@ -0,0 +1,7 @@
+package doranco;
+
+public class Main {
+    public static void main(String[] args) {
+        // Utilisez cette classe pour tester vos méthodes BookDAOImpl
+    }
+}
