3a029a3b6302bc90f839571c3b9b0d418e0c7f6a
MiC0901205 <m.pecheur09@gmail.com>
2024-02-21 14:52:16 +0100

    Fin du tp

--- src/main/java/doranco/service/CommandLine.java
+++ src/main/java/doranco/service/CommandLine.java
@@ -2,6 +2,7 @@ package doranco.service;
 
 import doranco.dao.BookDaoImpl;
 import doranco.entity.Book;
+import doranco.exception.NotFoundEntityException;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -9,40 +10,110 @@ import java.util.Scanner;
 
 public class CommandLine {
 
-    private Scanner sc;
-    private BookDaoImpl bookDao;
+	 private final Scanner sc;
+	    private final BookDaoImpl bookDao;
 
+	    public CommandLine() throws SQLException {
+	        sc = new Scanner(System.in);
+	        bookDao = new BookDaoImpl();
+	    }
 
-    public CommandLine() throws SQLException {
-        sc = new Scanner(System.in);
-        bookDao = new BookDaoImpl();
-    }
+	    public void start() throws NotFoundEntityException {
+	        int cmd;
+	        do {
+	            printOption();
+	            cmd = sc.nextInt();
+	            switch (cmd) {
+	                case 1:
+	                    displayBooks();
+	                    break;
+	                case 2:
+	                    searchBookByTitle();
+	                    break;
+	                case 3:
+	                    addBook();
+	                    break;
+	                case 4:
+	                    deleteBook();
+	                    break;
+	                case 5:
+	                    break;
+	                default:
+	                    System.out.println("Choix invalide !");
+	            }
+	        } while (cmd != 5);
+	        sc.close();
+	        System.out.println("Application fermée.");
+	    }
 
-    public void start() throws SQLException {
-        int cmd = 0;
-        do {
-            printOption();
-            cmd = sc.nextInt();
-            if (cmd == 1) {
-                displayBooks();
-            }
-        } while (cmd != 5);
-        sc.close();
-        System.out.println("Application closed");
-    }
+	    private void displayBooks() {
+	        try {
+	            List<Book> books = bookDao.findAll();
+	            if (books != null) {
+	                for (Book book : books) {
+	                    System.out.println("ID : " + book.getId());
+	                    System.out.println("Title : " + book.getTitle());
+	                    System.out.println("Year : " + book.getYear());
+	                    System.out.println("Author ID : " + book.getAuthorId());
+	                    System.out.println("----------");
+	                }
+	            } else {
+	                System.out.println("Aucun livre trouvé.");
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
 
-    private void displayBooks() throws SQLException {
-        List<Book> books = bookDao.findAll();
-        for (Book book: books)
-        {
-            System.out.print("ID : " + book.getId());
-            System.out.print("\tTitle : " + book.getTitle());
-            System.out.print("\tYear : " + book.getYear());
-            System.out.println("\tAuthor ID : " + book.getAuthorId());
-            System.out.println("----------");
-        }
-    }
+	    private void searchBookByTitle() {
+	        System.out.print("Entrez le titre du livre à rechercher : ");
+	        sc.nextLine(); // Pour consommer le saut de ligne
+	        String title = sc.nextLine();
+	        try {
+	            List<Book> books = bookDao.searchByTitle(title);
+	            if (books != null) {
+	                for (Book book : books) {
+	                    System.out.println("ID : " + book.getId());
+	                    System.out.println("Title : " + book.getTitle());
+	                    System.out.println("Year : " + book.getYear());
+	                    System.out.println("Author ID : " + book.getAuthorId());
+	                    System.out.println("----------");
+	                }
+	            } else {
+	                System.out.println("Aucun livre trouvé avec le titre : " + title);
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
 
+	    private void addBook() throws NotFoundEntityException {
+	        System.out.print("Entrez le titre du livre : ");
+	        sc.nextLine(); // Pour consommer le saut de ligne
+	        String title = sc.nextLine();
+	        System.out.print("Entrez l'année de publication : ");
+	        int year = sc.nextInt();
+	        System.out.print("Entrez l'ID de l'auteur : ");
+	        int authorId = sc.nextInt();
+	        Book book = new Book(title, year, authorId);
+	        try {
+	            bookDao.create(book);
+	            System.out.println("Livre ajouté avec succès !");
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+	    private void deleteBook() {
+	        System.out.print("Entrez l'ID du livre à supprimer : ");
+	        int id = sc.nextInt();
+	        try {
+	            bookDao.delete(id);
+	            System.out.println("Livre supprimé avec succès !");
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
     private void printOption()
     {
         System.out.print(
